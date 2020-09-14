291a6bd5878cd3601568a6c196b8b90457cba41b
Guillaume Langlois <glanglois@dept-info.crosemont.quebec>
2020-09-14 16:24:53 -0400

    ajout des exception dans l'intéracteur de céreation d'utilsaiteur

--- app/src/main/java/com/example/brawler/domaine/intéracteur/CréerUtilsateur.java
+++ app/src/main/java/com/example/brawler/domaine/intéracteur/CréerUtilsateur.java
@@ -5,14 +5,27 @@ import com.example.brawler.domaine.entité.Utilisateur;
 
 public class CréerUtilsateur {
 
-    public Utilisateur créerUnUtilisateur (String nom, Niveau niveau, String location) throws Exception {
+    public Utilisateur créerUnUtilisateur (String nom, Niveau niveau, String location, byte[] image) throws Exception {
         Boolean validation = true;
         String exception = "";
 
+        if(nom.trim().isEmpty() || nom.equals(null)) {
+            exception = "Le nom est vide";
+            validation = false;
+        } else if (niveau.equals(null)) {
+            exception = "l'utilsiateur n'a pas de niveau";
+            validation = false;
+        } else if (location.trim().isEmpty() || location.equals(null)) {
+            exception = "l'utilisateur n'à pas de location";
+            validation = false;
+        } else if (image.equals(null)) {
+            exception = "l'utilisateur n'à pas d'image";
+            validation = false;
+        }
         if(!validation) {
             throw new Exception(exception);
         }
-        return new Utilisateur(nom, niveau, location);
+        return new Utilisateur(nom, niveau, location, image);
     }
 
 }
