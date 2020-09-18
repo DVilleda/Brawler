175141bb85638fb10edbb2b4dd710cacb5f52e63
Guillaume Langlois <glanglois@dept-info.crosemont.quebec>
2020-09-17 20:29:32 -0400

    début du layout de recherche de match

--- app/src/main/java/com/example/brawler/présentation/présenteur/PrésenteurRechercheMatch.java
+++ app/src/main/java/com/example/brawler/présentation/présenteur/PrésenteurRechercheMatch.java
@@ -1,5 +1,17 @@
 package com.example.brawler.présentation.présenteur;
 
+import com.example.brawler.présentation.modèle.Modèle;
+import com.example.brawler.présentation.vue.VueRechercheMatch;
+
 public class PrésenteurRechercheMatch {
+
+    private VueRechercheMatch vue;
+    private Modèle modèle;
+
+    public PrésenteurRechercheMatch(VueRechercheMatch vue, Modèle modèle) {
+        this.vue = vue;
+        this.modèle = modèle;
+    }
+
 
 }
--- app/src/main/java/com/example/brawler/présentation/vue/VueRechercheMatch.java
+++ app/src/main/java/com/example/brawler/présentation/vue/VueRechercheMatch.java
@@ -1,6 +1,30 @@
 package com.example.brawler.présentation.vue;
 
-public class VueRechercheMatch {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
 
+import com.example.brawler.présentation.présenteur.PrésenteurRechercheMatch;
+
+public class VueRechercheMatch extends Fragment {
+
+    private PrésenteurRechercheMatch présenteur;
+
+    public void setPrésenteur(PrésenteurRechercheMatch présenteur) {
+        this.présenteur = présenteur;
+    }
+
+    @Override
+    public View onCreateView (LayoutInflater inflater,
+                              ViewGroup container,
+                              Bundle savedInstanceState) {
+
+        View vue = null;
+        
+        return vue;
+    }
+    
 }
